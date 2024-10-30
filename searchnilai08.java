f703727075529f9cd976a4ca81023b2c5eaf7923
Your Name <you@example.com>
2024-10-31 05:23:53 +0700

    modifikasi key yang dicari

--- searchnilai08.java
+++ searchnilai08.java
@@ -3,18 +3,31 @@ import java.util.Scanner;
 public class searchnilai08 {
     public static void main (String []args) {
 
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int jumlah = sc.nextInt();
+
+        int[] arrNilai = new int [jumlah];
+
+        for (int i=0 ; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i+1;
+                System.out.println();
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+                System.out.println();
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
     }
 }
